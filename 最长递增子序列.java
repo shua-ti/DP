0661f33e0a013d9740df78b2397e74f1d915c642
wanpeng16 <wanpengwx@163.com>
2016-12-21 16:22:00 +0800

    test

--- 最长递增子序列.java
+++ 最长递增子序列.java
@@ -1,9 +1,10 @@
+//求长递增子序列的长度
 public class Solution{
 	public int lenofLIS(int[] nums){
 		int[] dp =new int[nums.length];
 		int len=0;
 		for(int val : nums){
-             int idx=Array.binsearch(dp,0,len,val);
+             int idx=Array.binsearch(dp,0,len,val);//主要在于二分搜索模块
              if (idx<0) idx=-(idx+1);
              dp[idx]=val;
              if(idx==len) len++;
